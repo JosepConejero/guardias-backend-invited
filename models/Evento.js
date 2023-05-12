aef1424219660701aacd83433662640af1ccbced
JosepConejero <bermeu@gmail.com>
2023-05-12 19:34:37 +0200

    Removed _id from Evento.js

--- models/Evento.js
+++ models/Evento.js
@@ -50,12 +50,12 @@ const EventoSchema = Schema({
 
 //voy a sobreescribir el método toJSON que es un serializador,
 //que se aplica cuando grabo en la base de datos??
-EventoSchema.method("toJSON", function () {
+/* EventoSchema.method("toJSON", function () {
   //con esto solo sobreescribo el toJSON, q no usamos explícitamente pero sí lo usa mongoose implícitamente
   const { __v, _id, ...object } = this.toObject(); //extraeré __v, _id y todo lo demás va a estar almacenado en object
   object.id = _id;
   return object;
-});
+}); */
 //lo escribo con function pq necesito referencia al this, que hace refencia al objeto q estoy serializando
 //y de ahí extraeré varias cosas (con this tengo acceso a todas las ppdades)
 
