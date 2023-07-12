108e03b6dceae30544d9311a82ef05f7d2430783
JosepConejero <bermeu@gmail.com>
2023-07-12 20:32:26 +0200

    Added usuario.shortName to login and register

--- controllers/auth.js
+++ controllers/auth.js
@@ -66,6 +66,7 @@ const crearUsuario = async (req, res = response) => {
       uid: usuario.id, // esto es lo que quiero devolver en la response cuando cree un nuevo usuario
       name: usuario.name,
       token: token, // aquí podría ponerse solo token,
+      shortName: usuario.shortName,
       isAdmin: usuario.isAdmin,
       isActivated: usuario.isActivated,
       isDataModifier: usuario.isDataModifier,
@@ -118,6 +119,7 @@ const loginUsuario = async (req, res = response) => {
       uid: usuario.id, // esto es lo que quiero devolver en la response cuando cree un nuevo usuario
       name: usuario.name,
       token: token, // aquí podría ponerse solo token,
+      shortName: usuario.shortName,
       isAdmin: usuario.isAdmin,
       isActivated: usuario.isActivated,
       isDataModifier: usuario.isDataModifier,
