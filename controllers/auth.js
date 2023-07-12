3af7560ef841dbea807070f2f76d06671164079c
JosepConejero <bermeu@gmail.com>
2023-07-12 20:24:37 +0200

    Added shortName to response in renew

--- controllers/auth.js
+++ controllers/auth.js
@@ -179,6 +179,7 @@ const revalidarToken = async (req, res = response) => {
       uid,
       name,
       token,
+      shortName: usuario.shortName,
       isAdmin: usuario.idAdmin,
       isActivated: usuario.isActivated,
       isDataModifier: usuario.isDataModifier,
