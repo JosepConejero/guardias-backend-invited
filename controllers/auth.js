3e7c88f5d92480b4dc4447d1a77d4cce8b9b7c8b
JosepConejero <bermeu@gmail.com>
2023-07-12 19:18:04 +0200

    Added email to revalidarToken

--- controllers/auth.js
+++ controllers/auth.js
@@ -151,6 +151,7 @@ const revalidarToken = async (req, res = response) => {
   const {
     uid,
     name,
+    email,
     /*  isAdmin,
     isActivated,
     isDataModifier,
@@ -162,7 +163,7 @@ const revalidarToken = async (req, res = response) => {
   // ahora se genera un nuevo JWT y lo devuelve en esta petición
   const token = await generarJWT(uid, name);
   try {
-    const usuario = await Usuario.findOne({ uid });
+    const usuario = await Usuario.findOne({ email });
     if (!usuario) {
       return res.status(400).json({
         ok: false,
