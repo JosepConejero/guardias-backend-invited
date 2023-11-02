96d16eae58b1e692773f00fe56fb24187a4e8da4
JosepConejero <bermeu@gmail.com>
2023-11-02 12:34:50 +0100

    Removed comments in helpers/jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,29 +1,15 @@
 const jwt = require("jsonwebtoken");
-// esta librería trabaja con callbacks, no con promesas,
-// aq hay una mra d hacer q trabaje como queremos
 
-// en esta función debería recibir lo que yo
-// necesito colocar como payload de mi token
-// (una de las tres partes del JWT), es decir,
-// el id del usuario uid y el nombre name
 const generarJWT = (uid, name) => {
   return new Promise((resolve, reject) => {
     const payload = { uid, name };
-    jwt.sign(
-      payload,
-      process.env.SECRET_JWT_SEED,
-      //{ expiresIn: "2h" },
-      (err, token) => {
-        if (err) {
-          console.log(err);
-          reject("No se pudo generar el token"); //si no va bien, devuelve este mensaje de error
-        }
-        resolve(token); //si todo va bien, devuelve el token
+    jwt.sign(payload, process.env.SECRET_JWT_SEED, (err, token) => {
+      if (err) {
+        console.log(err);
+        reject("No se pudo generar el token");
       }
-    );
-    // le paso el payload, la palabra secreta,
-    // un objeto con una ppdad q indice cuándo expirará el token
-    // y por último una callback q se va a disparar cuando se produzca un error
+      resolve(token);
+    });
   });
 };
 
