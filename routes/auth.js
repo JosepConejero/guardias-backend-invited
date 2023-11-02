f129367c7cdce039c0143cf9c4fa423793725105
JosepConejero <bermeu@gmail.com>
2023-11-02 12:48:25 +0100

    Removed comments in routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,10 @@
 /*
     Rutas de Usuarios / Auth
-    host + /api/auth (es decir, localhost:..../api/auth)
+    host + /api/auth 
 */
 
-// puedo hacerlo de una de estas dos formas:
-// const express = require("express");
-// const router = express.Router();
-
-const { Router } = require("express"); //desestructuro Router
-const { check } = require("express-validator"); //desestructuraré de express-validator
+const { Router } = require("express");
+const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
 const {
   crearUsuario,
@@ -21,27 +17,22 @@ const { validarJWT } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
-//usaré router en vez de app
 router.post(
   "/new",
   [
-    //mis middlewares
     check("name", "El nombre es obligatorio").not().isEmpty(),
     check("email", "El email es obligatorio").isEmail(),
     check("password", "El password ha de ser de 6 caracteres").isLength({
       min: 6,
     }),
-    validarCampos, //será otro middleware con su next
+    validarCampos,
   ],
   crearUsuario
 );
-//si hiciera un post, lo que devuelve res.json sería lo que quiero
-//que devuelva cuando haga ese posteo
 
 router.post(
   "/",
   [
-    //mis middlewares
     check("email", "El email es obligatorio").isEmail(),
     check("password", "El password ha de ser de 6 caracteres").isLength({
       min: 6,
@@ -51,7 +42,6 @@ router.post(
   loginUsuario
 );
 
-// si donde va validarJWT fuera más de un middleware, lo pondría entre []
 router.get("/renew", validarJWT, revalidarToken);
 
 router.patch("/:id", restaurarPasswordUsuario);
@@ -59,8 +49,6 @@ router.patch("/:id", restaurarPasswordUsuario);
 router.patch(
   "/",
   [
-    //mis middlewares
-    //check("email", "El email es obligatorio").isEmail(),
     check("password", "El password ha de ser de 6 caracteres").isLength({
       min: 6,
     }),
