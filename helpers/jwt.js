9a02b1f7dfcf247b40464d200ed078884edc2a32
JosepConejero <bermeu@gmail.com>
2023-11-02 13:04:32 +0100

    Removed console.log from helpers/jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -5,7 +5,6 @@ const generarJWT = (uid, name) => {
     const payload = { uid, name };
     jwt.sign(payload, process.env.SECRET_JWT_SEED, (err, token) => {
       if (err) {
-        console.log(err);
         reject("No se pudo generar el token");
       }
       resolve(token);
