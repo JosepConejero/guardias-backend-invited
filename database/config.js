717e2627fc4b80089295d040d0b8aabedd0af3e4
JosepConejero <bermeu@gmail.com>
2023-11-02 12:31:18 +0100

    Removed comments in database/config.js

--- database/config.js
+++ database/config.js
@@ -2,14 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(
-      process.env
-        .DB_CONNECTION /* , {//esto no funciona pq la versión de mongoose es distinta pero fernando ha añadido el useCreateIndex: true
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    } */
-    ); // todo esto retornará una promesa, por eso pongo lo del await
+    await mongoose.connect(process.env.DB_CONNECTION);
     console.log("DB online");
   } catch (error) {
     console.log(error);
