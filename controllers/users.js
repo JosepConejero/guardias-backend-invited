968b4d84bb5ddf5b6095fcb91fe97085bb652919
JosepConejero <bermeu@gmail.com>
2023-11-02 12:29:32 +0100

    Removed comments in controllers/users.js

--- controllers/users.js
+++ controllers/users.js
@@ -2,42 +2,17 @@ const { response } = require("express");
 const Usuario = require("../models/Usuario");
 
 const getUsuarios = async (req, res = response) => {
-  const usuarios = await Usuario.find(); //en el find puedo especificar condiciones con {loquesea}, pero como quiero traerlos todos, no pongo nada dentro de ()
+  const usuarios = await Usuario.find();
   res.json({
     ok: true,
     usuarios,
   });
 };
 
-/* const crearCurso = async (req, res = response) => {
-  //aquí verificaré que en el body tengo el curso
-  // en postman lo puedo probar poniendo algo en el body en formato json
-  //console.log(req.body);
-
-  const curso = new Curso(req.body); //ya tengo una nueva instancia de mi modelo
-
-  try {
-    const cursoGuardado = await curso.save();
-    res.status(200).json({
-      ok: true,
-      curso: cursoGuardado,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      ok: false,
-      msg: "Hable con el administrador",
-    });
-  }
-}; */
-
 const actualizarUsuario = async (req, res = response) => {
-  //primero tomo el valor del id que viene en la url
   const usuarioId = req.params.id;
 
   try {
-    //primero compruebo q el usuario existe
-    //console.log(usuarioId);
     const usuario = await Usuario.findById(usuarioId);
 
     if (!usuario) {
@@ -49,7 +24,6 @@ const actualizarUsuario = async (req, res = response) => {
     const nuevoUsuario = {
       ...req.body,
     };
-    //ahora lo grabo en la bda
     const usuarioActualizado = await Usuario.findByIdAndUpdate(
       usuarioId,
       nuevoUsuario,
@@ -60,7 +34,6 @@ const actualizarUsuario = async (req, res = response) => {
       usuario: usuarioActualizado,
     });
   } catch (error) {
-    //este error en teoría no tiene por qué aparecer, pero podría personalizarlo también con la hora, etc.
     console.log(error);
     res.status(500).json({
       ok: false,
@@ -70,11 +43,9 @@ const actualizarUsuario = async (req, res = response) => {
 };
 
 const eliminarUsuario = async (req, res = response) => {
-  //primero tomo el valor del id que viene en la url
   const usuarioId = req.params.id;
 
   try {
-    //primero compruebo q el usuario existe
     const usuario = await Usuario.findById(usuarioId);
 
     if (!usuario) {
@@ -84,14 +55,12 @@ const eliminarUsuario = async (req, res = response) => {
       });
     }
 
-    //ahora lo elimino de la bda
     await Usuario.findByIdAndDelete(usuarioId);
     res.json({
       ok: true,
       usuarioIdBorrado: usuarioId,
     });
   } catch (error) {
-    //este error en teoría no tiene por qué aparecer, pero podría personalizarlo también con la hora, etc.
     console.log(error);
     res.status(500).json({
       ok: false,
@@ -101,7 +70,7 @@ const eliminarUsuario = async (req, res = response) => {
 };
 
 module.exports = {
-  getUsuarios /* , crearUsuario */,
+  getUsuarios,
   actualizarUsuario,
   eliminarUsuario,
 };
