0dd95641e7df78aa73d5a214c1a0397bd6c2610a
JosepConejero <bermeu@gmail.com>
2023-11-02 12:52:45 +0100

    Removed comments in routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,58 +1,28 @@
 /*
     Users routes / Rutas de usuarios
-    host + /api/users (es decir, localhost:..../api/routes)
+    host + /api/users
 */
 
-const { Router } = require("express"); //Router es una función que me permite generar el router
+const { Router } = require("express");
 const { validarJWT } = require("../middlewares/validar-jwt");
 const router = Router();
 
-const { check } = require("express-validator"); //desestructuraré de express-validator
+const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validar-campos");
 const {
   getUsuarios,
-  /*   crearUsuario, */
   actualizarUsuario,
   eliminarUsuario,
 } = require("../controllers/users");
 
-// Todas tienen que pasar por la validación del JWT
-// Lo puedo hacer poniendo el middleware validarJWT en cada una de ellas así: router.get("/", validarJWT, getUsuarios);
-// o bien poniéndolo antes de manera que le estoy diciendo
-// que primero pasa por este middleware y después por los get, post, put y delete posteriores
-
 router.use(validarJWT);
 
-// si le cambio la ubicación a este use(validarJWT), p.e., si lo pongo detrás del get, todo el mundo podrá hacer este get
-// pero para el resto se tendrá que estar validado;
-// la ejecución va por orden: para cada petición http, primero valida con use(validarJWT) y dps atiende el get, post o lo q sea
+router.get("/", getUsuarios);
 
-// Obtener usuarios
-router.get("/", getUsuarios); // Le voy a hacer una petición al /
-
-// Crear un nuevo usuario
-/* router.post(
-  "/",
-  [
-    check("title", "El título es obligatorio").not().isEmpty(), //el .not().isEmpty() es para que siempre haya información
-    check("frequent", "Es obligatorio especificar si el curso es frecuente")
-      .not()
-      .isEmpty(),
-    check("flc", "Es obligatorio especificar si el curso es de la FLC")
-      .not()
-      .isEmpty(),
-    validarCampos,
-  ],
-  crearUsuario
-); */
-
-//router.use(validarJWT); (si lo pusiera aquí, todo lo q va después exigirá autenticación)
-
-// Actualizar usuario (junto al / podrá haber cualquier cosa; p.e. /23452345234)
 router.put(
   "/:id",
   [
-    check("name", "El nombre es obligatorio").not().isEmpty(), //el .not().isEmpty() es para que siempre haya información
+    check("name", "El nombre es obligatorio").not().isEmpty(),
     check(
       "shortName",
       "Es obligatorio especificar el nombre breve del trabajador"
@@ -110,7 +80,6 @@ router.put(
   actualizarUsuario
 );
 
-// Borrar usuario
 router.delete("/:id", eliminarUsuario);
 
 module.exports = router;
